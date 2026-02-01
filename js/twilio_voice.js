fd6182000f094937ccdf79b8bde6f29386b0cfad
annevjenkins22a <annevjenkins22@gmail.com>
2026-02-01 22:45:06 +0000

    Update twilio_voice.js

--- js/twilio_voice.js
+++ js/twilio_voice.js
@@ -403,12 +403,14 @@ const response = await fetch( TENEO_ENGINE_URL, {
 teneoResponse = await response.json();
 console.log(_stringify(teneoResponse));
 var xgs = response.headers.get("X-Gateway-Session");
+ const result =  xgs.slice( xgs.indexOf(";") + 1);
 var sessionId=teneoResponse.sessionId;
- console.log("xgs=" + xgs);
-if(teneoSessionId.includes(xgs)) {
+ console.log("xgs=" + result);
+if(teneoSessionId.includes("ApplicationGatewayAffinityCORS")) {
+    console.log("keeping teneoSessionId=" + teneoSessionId);
 }
 else {
-    teneoSessionId="JSESSIONID="+sessionId+";"+xgs
+    teneoSessionId="JSESSIONID="+sessionId+";"+result
      console.log("setting teneoSessionId=" + teneoSessionId);
 }
 
